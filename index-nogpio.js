8f79640372d6ea6976d61debcc7721e2a139c4eb
Aleksis Brezas <abresas@resin.io>
2014-08-14 15:54:37 +0000

    fix led file error

--- index-nogpio.js
+++ index-nogpio.js
@@ -5,8 +5,8 @@ var LED_BRIGHTNESS = '/sys/class/leds/led0/brightness';
 fs.writeFileSync(LED_TRIGGER, 'none');
 
 var blinkInterval = setInterval(function(){
-  fs.writeFileSync(LED_FILE, '1');
-  setTimeout(function() { fs.writeFileSync(LED_FILE, '0') }, 500);
+  fs.writeFileSync(LED_BRIGHTNESS, '1');
+  setTimeout(function() { fs.writeFileSync(LED_BRIGHTNESS, '0') }, 500);
 }, 1000)
 
 process.on('exit', function(){
